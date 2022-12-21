9fe9a1ce8745a5c6f1226bb2dd51518c3119d720
npham49 <nguyenphamswork@gmail.com>
2022-12-20 23:13:04 -0800

    added codebase

---
+++ ecommerce-backend/schemas/product.ts
@@ -0,0 +1,45 @@
+export default {
+
+  //this is the display name for the type
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: {
+        //helps with better center the image position on the page
+        hotspot: true,
+      }
+    },
+    //this is the display name for every items
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        //generate a slug from the product name
+        source: 'name',
+        maxLength: 90,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    }
+  ]
+}
